97a29a6578676c4698f9ee93b1f3db6dd48cb8de
skyvault <dustinneumann42@gmail.com>
2018-08-08 13:47:36 -0700

    Application class now works

--- application.py
+++ application.py
@@ -11,28 +11,39 @@ Dependencies:
     pillow
 """
 
-def openImage():
-    pass
+class Application():
+    def __init__(self, size):
+        self.inImgData = None
 
+        self.root = tk.Tk()
+        self.root.geometry("{}x{}".format(size[0], size[1]))
+
+        self.inLabel = tk.Label(self.root, text="No Image")
+
+        # Menu bar
+        self.menubar = tk.Menu(self.root)
+        self.root.config(menu=self.menubar)
+
+        self._file = tk.Menu(self.menubar)
+        self._file.add_command(label="Open Image", command=self.openImage)
+        self.menubar.add_cascade(label="File", menu=self._file)
+
+        self.root.mainloop()
+
+    def openImage(self):
+        result = tkFileDialog.askopenfilename(initialdir="", title="Select Image", filetypes=(("Jpeg files", "*.jpg *.jpeg"), ("Png files", "*.png"), ("all files", "*.*")))
+        if result:
+            self.inImgData = Image.open(result)
+            self.inImgData = self.inImgData.resize((600, 600))
+
+            imgtk = ImageTk.PhotoImage(self.inImgData)
+
+            self.inLabel.config(image=imgtk)
+            self.inLabel.imgtk = imgtk
+            self.inLabel.pack_forget()
+            self.inLabel.pack()
+        
 def run():
-    root = tk.Tk()
-    root.geometry("1200x600")
-    root.grid()
-
-    menubar = tk.Menu(root)
-    root.config(menu=menubar)
-
-    _file = tk.Menu(menubar)
-    _file.add_command(label="Open Image", command=openImage)
-    menubar.add_cascade(label="File", menu=_file)
-
-    img = Image.open("percent4m.jpeg")
-    img = img.resize((600, 600))
-
-    imgtk = ImageTk.PhotoImage(img)
-    label = tk.Label(root, image=imgtk)
-    label.grid(row=0, column=0)
-    # canvas.create_image(0, 0, anchor=tk.NW, image=imgtk)
-    root.mainloop()
+    Application((1200, 600))
 
 if __name__=="__main__": run()
